77edb9abc3954518e3cb1096116ce7787a4c2b3b
Błażej Drozd <drozdbazej@gmail.com>
2024-06-01 22:23:44 +0200

    set Jikan as default metadata search engine

--- src/ShindenSearchCore.ts
+++ src/ShindenSearchCore.ts
@@ -6,8 +6,11 @@ export class ShindenSearchCore {
     metadataProvider: ISearchEngine = new JikanSearchEngine();
     metadataLanguage: Languages;
     // TODO: A lot ＼(ﾟｰﾟ＼)
-    constructor(_metadataProvider: ISearchEngine) {
-        this.metadataProvider = _metadataProvider;
+    constructor(_metadataProvider?: ISearchEngine) {
+        if(_metadataProvider) {
+            this.metadataProvider = _metadataProvider;
+        }
+        
         this.metadataLanguage = this.metadataProvider.language;
     }
 };
